cda4a0398b65d30207101d1b4fc639d4b8c3ddc1
zhangxin774 <zhangxin774@jd.com>
2020-09-10 21:44:39 +0800

    //初始版

---
+++ jsl-dd-workbench-service/src/main/java/com/jd/jsl/dd/service/service/impl/LoginServiceImpl.java
@@ -0,0 +1,13 @@
+package com.jd.jsl.dd.service.service.impl;
+
+import com.jd.jsl.dd.service.service.LoginService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wujia215
+ * @date 2020/9/1021:17
+ */
+
+@Service
+public class LoginServiceImpl implements LoginService {
+}
